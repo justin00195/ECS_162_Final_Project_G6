ef8c4bb758fe286f77d1df8aa0c84fbe7538f008
patple <152749587+patple@users.noreply.github.com>
2025-05-26 18:11:09 -0700

    made some UI changes

--- frontend/src/routes/Homepage.svelte
+++ frontend/src/routes/Homepage.svelte
@@ -77,9 +77,8 @@ function userLogout() {
             <a use:link href="/user-portal">User Portal</a>
         {/if}
       </nav>
-      
-      
-      {#if currentPage === 'calculator'}
+    </header>
+    {#if currentPage === 'calculator'}
         <CalculatorPage />
       {:else if currentPage === 'goal'}
         <GoalPage />
@@ -90,22 +89,22 @@ function userLogout() {
       {:else if currentPage === 'recipe'}
         <RecipePage />
       {/if}
-    </header>
-  
+  {#if currentPage === 'home'} 
     <h1 class="Welcome">Welcome to Food Tracker</h1>
-
-    <div class="grid">
-      <div class="card">
-        <h3>About Us</h3>
-        <p>College students struggle with healthy eating. We make it easier.</p>
+      <div class="grid">
+        <div class="card">
+          <h3>About Us</h3>
+          <p>College students struggle with healthy eating. We make it easier.</p>
+        </div>
+        <div class="card">
+          <h3>What to Expect</h3>
+          <p>Get daily, personalized meal plans based on your goals and preferences.</p>
+        </div>
+        <div class="card" style="flex-basis: 100%;">
+          <h3>Track Your Progress</h3>
+          <p>Stay motivated by tracking weight, goals, and progress over time.</p>
+        </div>
       </div>
-      <div class="card">
-        <h3>What to Expect</h3>
-        <p>Get daily, personalized meal plans based on your goals and preferences.</p>
-      </div>
-      <div class="card" style="flex-basis: 100%;">
-        <h3>Track Your Progress</h3>
-        <p>Stay motivated by tracking weight, goals, and progress over time.</p>
-      </div>
-    </div>
-  </div>
+  {/if}
+</div>
+    
--- frontend/src/routes/ReportPage.svelte
+++ frontend/src/routes/ReportPage.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import { onMount } from 'svelte';
+    import {Tween} from 'svelte/motion'
+    import { cubicOut } from 'svelte/easing';
   
     let report: any = {};
   
@@ -7,8 +9,52 @@
       const res = await fetch('http://localhost:8000/report');
       report = await res.json();
     });
+
+
+    let progress = new Tween(0, {
+      duration: 500,
+      easing: cubicOut
+    })
+    let inputValue = 0;
+    function updateProgress(){
+      let val = Math.min(100, Math.max(0, Number(inputValue)))
+      progress.target = val
+    }
   </script>
   
-  <h1>Report Page</h1>
-  <pre>{JSON.stringify(report, null, 2)}</pre>
   
+<pre>{JSON.stringify(report, null, 2)}</pre>
+  <header>
+    <h1>Report Page</h1>
+  </header>
+    
+    <div class="progress-container">
+      <progress max = "100" value={progress.current}></progress>
+      <input
+        type ="number"
+        bind:value={inputValue}
+        min ="0"
+        max ="100"
+        on:change={updateProgress}
+      />
+    </div>
+  
+  
+  <!---Subject to change-->
+  <style>
+
+    .progress-container {
+      display: flex;
+      justify-content: center;
+      width: 500px;
+      margin: 5rem auto 0 auto;
+    }
+    progress{
+      width: 100%;
+      height: 30px;
+      appearance: none;
+      border-radius: 15px;
+      overflow: hidden;
+      
+    }
+  </style>
