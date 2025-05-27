813adb6e736eb9097d219376d01cdace4a98304a
patple <152749587+patple@users.noreply.github.com>
2025-05-26 18:50:01 -0700

    Update ReportPage.svelte

--- frontend/src/routes/ReportPage.svelte
+++ frontend/src/routes/ReportPage.svelte
@@ -4,6 +4,13 @@
     import { cubicOut } from 'svelte/easing';
   
     let report: any = {};
+
+    // calorieBudget should be calcualted from the goalpage
+    let calorieBudget = 1000;
+    let breakFast = 0;
+    let lunch = 0;
+    let dinner = 0;
+    let snacks = 0;
   
     onMount(async () => {
       const res = await fetch('http://localhost:8000/report');
@@ -15,10 +22,12 @@
       duration: 500,
       easing: cubicOut
     })
-    let inputValue = 0;
+    $: calsAte = breakFast + lunch + dinner + snacks;
+    $: calsLeft = Math.max(0, calorieBudget - calsAte)
+
     function updateProgress(){
-      let val = Math.min(100, Math.max(0, Number(inputValue)))
-      progress.target = val
+      const totalCals = breakFast + lunch + dinner + snacks;
+      progress.target = Math.min(calorieBudget, Math.max(0,totalCals))
     }
   </script>
   
@@ -28,26 +37,90 @@
     <h1>Report Page</h1>
   </header>
     
-    <div class="progress-container">
-      <progress max = "100" value={progress.current}></progress>
-      <input
-        type ="number"
-        bind:value={inputValue}
-        min ="0"
-        max ="100"
-        on:change={updateProgress}
+<div class="progress-container">
+  <div class ="progress-wrapper">
+    <div class ="progress-label">Daily Calorie Budget</div>
+    <progress max = {calorieBudget} value={progress.current}></progress>
+
+
+  <div class ="progress-info">
+    <div>Daily Budget:{calorieBudget} | Calories Eaten: {calsAte} | Budget Remaining:{calsLeft}</div> 
+  </div>
+  </div>
+</div>
+
+<div class = "input-box">
+  <div>
+    <label for = "breakFastLabel">Breakfast</label>
+    <input
+      id ="breakFastLabel"
+      type ="number"
+      bind:value={breakFast}
+      on:change={updateProgress}
       />
-    </div>
+  </div>
+    
+  <div>
+    <label for = "lunchLabel">Lunch</label>
+    <input
+      id ="lunchLabel"
+      type ="number"
+      bind:value={lunch}
+      on:change={updateProgress}
+      />
+  </div>
+
+   <div>
+    <label for = "dinnerLabel">Dinner</label>
+    <input
+      id ="dinnerLabel"
+      type ="number"
+      bind:value={dinner}
+      on:change={updateProgress}
+      />
+  </div>
+    
+  <div>
+    <label for = "snacksLabel">Snacks</label>
+    <input
+      id ="snacksLabel"
+      type ="number"
+      bind:value={snacks}
+      on:change={updateProgress}
+      />
+  </div>
+</div>
+     
   
   
   <!---Subject to change-->
   <style>
 
+    .input-box{
+      display: grid;
+      grid-template-columns: repeat(2,1fr);
+      gap:1rem;
+      max-width: 400px;
+      margin: 1rem auto;
+      background-color: #DFD3CC;
+      border-radius: 10px;
+      padding: 2rem;
+    }
+    .progress-wrapper{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 500px;
+    }
+    .progress-label{
+      font-weight: bold;
+      text-align: center;
+      margin-bottom: 0.5rem;
+    }
     .progress-container {
       display: flex;
       justify-content: center;
-      width: 500px;
-      margin: 5rem auto 0 auto;
+      margin: 1rem auto 0 auto;
     }
     progress{
       width: 100%;
